be802194c8fcd81787f7cc96ce2316e0aee37338
Dave Hudson <dave@applification.net>
2022-02-02 06:55:44 +0000

    ported over turborepo code

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,37 +1,3 @@
-import { Link, useLoaderData } from "remix";
-import { getUser } from "~/utils/session.server";
-
-export const loader = async ({ request }: { request: Request }) => {
-  const user = await getUser(request);
-  const data = {
-    user,
-  };
-  return data;
-};
-
 export default function Index() {
-  const { user } = useLoaderData();
-  return (
-    <div>
-      <h1 className="text-pink-700 text-4xl">Welcome</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="posts">Posts</Link>
-          </li>
-          {user ? (
-            <li>
-              <form action="/auth/logout" method="POST">
-                <button type="submit">Logout {user.username}</button>
-              </form>
-            </li>
-          ) : (
-            <li>
-              <Link to="/auth/login">Login</Link>
-            </li>
-          )}
-        </ul>
-      </nav>
-    </div>
-  );
+  return <div>Layout route?</div>;
 }
--- app/routes/posts.tsx
+++
@@ -1,5 +0,0 @@
-import { Outlet } from "remix";
-
-export default function Posts() {
-  return <Outlet />;
-}
